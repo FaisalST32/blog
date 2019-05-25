2b5f3c9eaddc383d51d300c1b3008ffab7f6611b
Faisal Rashid <faisalrashid@LIVE.IN>
2019-05-25 17:17:46 +0530

    Added Environment

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = 'http://localhost:5000/api/auth/';
+  private baseUrl = environment.serverUrl + 'auth/';
   private token: string;
 
   constructor(private http: HttpClient) { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  serverUrl: 'https://apifr.azurewebsites.net/api/'
 };
